ca0d623f2e7a83fc0a17023469338741ea16e705
MihailTr <43892857+MihailTr@users.noreply.github.com>
2018-11-15 16:03:14 +0200

    Update example_webscraping_com.py
    Написати програму яка виведе всі країни зображені на цій сторінці http://example.webscraping.com/ (лише на першій сторінці)
    Доработка кода до рабочего состояния.

--- example_webscraping_com.py
+++ example_webscraping_com.py
@@ -8,27 +8,29 @@ from urllib.request import urlopen, Request
 import html
 
 acco_url = "http://example.webscraping.com/"
-headers={"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:63.0)"}
-acco_request=Request(acco_url, headers=headers)
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:63.0)"}
+acco_request = Request(acco_url, headers=headers)
 acco_page = urlopen(acco_request).read()
-acco_page=str(acco_page.decode())
-acco_page_len=len(acco_page)
-#print(acco_page)
-"""
-Temperature
-"""
-acco_teg_f='.png" />'
-acco_teg_f_size=len(acco_teg_f)
-acco_teg_f_index=acco_page.find(acco_teg_f)
-acco_teg_f_start=acco_teg_f_index +acco_teg_f_size
-acco_teg=''
+acco_page = str(acco_page.decode())
 
-#for i in range(acco_page_len):
-for char in acco_page[acco_teg_f_start:]:
+acco_page_count=acco_page.count('.png" />')
+char_res = ""
+
+for i in range(acco_page_count):
+    acco_teg_f = '.png" />'
+    acco_teg_f_size = len(acco_teg_f)
+    acco_teg_f_index = acco_page.find(acco_teg_f)
+    acco_teg_f_start = acco_teg_f_index +acco_teg_f_size
+    acco_teg=''
+    cout = 0
+
+    for char in acco_page[acco_teg_f_start:]:
         if char != '<':
-            acco_teg+=char
+            acco_teg += char
+            cout += 1
         else:
+            acco_page = acco_page[(acco_teg_f_start+cout):]
+            char_res += acco_teg
             break
 
-
-print("for: ", acco_teg, "\n")
+print("for: ", char_res, "\n")
