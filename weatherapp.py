1db85887c4abac4d110565b67c125f95e2b72a37
MihailTr <tmyfly@gmail.com>
2018-11-21 21:44:01 +0200

    Исправлени HTML teg для поиска Cond.

--- weatherapp.py
+++ weatherapp.py
@@ -6,23 +6,23 @@ from urllib.request import urlopen, Request
 import html
 
 acco_url = "https://www.accuweather.com/ru/ua/vinnytsia/326175/weather-forecast/326175"
-headers={"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:63.0)"}
-acco_request=Request(acco_url, headers=headers)
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:63.0)"}
+acco_request = Request(acco_url, headers=headers)
 acco_page = urlopen(acco_request).read()
-acco_page=str(acco_page.decode())
+acco_page = str(acco_page.decode())
 
 """
 Temperature
 """
-acco_teg_f='<span class="large-temp">'
-acco_teg_f_size=len(acco_teg_f)
-acco_teg_f_index=acco_page.find(acco_teg_f)
-acco_teg_f_start=acco_teg_f_index+acco_teg_f_size
-acco_teg=''
+acco_teg_f = '<span class="large-temp">'
+acco_teg_f_size = len(acco_teg_f)
+acco_teg_f_index = acco_page.find(acco_teg_f)
+acco_teg_f_start = acco_teg_f_index + acco_teg_f_size
+acco_teg = ''
 
 for char in acco_page[acco_teg_f_start:]:
     if char != '<':
-        acco_teg+=char
+        acco_teg += char
     else:
         break
 
@@ -41,7 +41,6 @@ for char in acco_page[acco_Cond_f_start:]:
     else:
         break
 
-
 """
 Current_city
 """
@@ -58,62 +57,54 @@ for char in acco_page[acco_Current_city_f_start:]:
     else:
         break
 
-
 """
 http://rp5.ua/
 """
 rp5_url = ('http://rp5.ua/%D0%9F%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0_%D0%B2_%D0'
-            '%92%D0%B8%D0%BD%D0%BD%D0%B8%D1%86%D0%B5')
-headers={"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:63.0)"}
-rp5_request=Request(rp5_url, headers=headers)
+           '%92%D0%B8%D0%BD%D0%BD%D0%B8%D1%86%D0%B5')
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:63.0)"}
+rp5_request = Request(rp5_url, headers=headers)
 rp5_page = urlopen(rp5_request).read()
-rp5_page=rp5_page.decode()
+rp5_page = rp5_page.decode()
 
 """
 Temperature
 """
-rp5_teg_f='<div id="ArchTemp"><span class="t_0" style="display: block;">'
-rp5_teg_f_size=len(rp5_teg_f)
-rp5_teg_f_index=rp5_page.find(rp5_teg_f)
-rp5_teg_f_start=rp5_teg_f_index+rp5_teg_f_size
-rp5_teg=''
+rp5_teg_f = '<div id="ArchTemp"><span class="t_0" style="display: block;">'
+rp5_teg_f_size = len(rp5_teg_f)
+rp5_teg_f_index = rp5_page.find(rp5_teg_f)
+rp5_teg_f_start = rp5_teg_f_index + rp5_teg_f_size
+rp5_teg = ''
 
 for char in rp5_page[rp5_teg_f_start:]:
     if char != '<':
-        rp5_teg+=char
+        rp5_teg += char
     else:
         break
-
 
 """
 Cond
 """
-rp5_Cond_f = '<tr class="underlineRow"><td colspan="2" class=" litegrey n"><div style="width: 100%; height: 100%" onmouseover="tooltip(this, '
+rp5_Cond_f = '<div class="cn3" onmouseover="tooltip(this,' + " '<b>"
+print(rp5_Cond_f)
 rp5_Cond_f_size = len(rp5_Cond_f)
 rp5_Cond_f_index = rp5_page.find(rp5_Cond_f)
 rp5_Cond_f_start = rp5_Cond_f_index + rp5_Cond_f_size
 rp5_Cond = ''
 
 for char in rp5_page[rp5_Cond_f_start:]:
-    if char != ",":
+    if char != "<":
         rp5_Cond += char
     else:
         break
 
-rp5_Cond=rp5_Cond[1:-2]
-
-
-
-
-
-
-acco_teg=html.unescape(acco_teg)
-rp5_teg=html.unescape(acco_teg)
+acco_teg = html.unescape(acco_teg)
+rp5_teg = html.unescape(acco_teg)
 
 print("Accuweather for: ", acco_Current_city, "\n")
-print("Temperature:     ",acco_teg,"\n")
-print("Cond:            ",acco_Cond,"\n")
+print("Temperature:     ", acco_teg, "\n")
+print("Cond:            ", acco_Cond, "\n")
 
-print("rp5 for: ")#, rp5_Current_city, "\n")
-print("Temperature:     ",rp5_teg,"\n")
-print("Cond:            ",rp5_Cond,"\n")
+print("rp5 for: ")  # , rp5_Current_city, "\n")
+print("Temperature:     ", rp5_teg, "\n")
+print("Cond:            ", rp5_Cond, "\n")
